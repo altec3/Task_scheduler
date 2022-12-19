11b0aa6f9182aeab34018dde05f3bf107cee9113
Aleksey Lesnikov <poprobuyu@mail.ru>
2022-12-19 18:40:46 +0500

    Create custom user model

--- todolist/core/models.py
+++ todolist/core/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ['username']
+
+    def __str__(self):
+        return self.username
