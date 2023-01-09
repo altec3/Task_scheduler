b3f0d6eb9d70570c601767595b43b0e3c7db8d79
Aleksey Lesnikov <poprobuyu@mail.ru>
2023-01-09 18:15:49 +0500

    Install DRF & DRF-Simplejwt

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
 
     # Local
     'core.apps.CoreConfig',
