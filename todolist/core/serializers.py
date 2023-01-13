25469d58061aa818e57df3e9d60d0d399fa3c05c
Aleksey Lesnikov <poprobuyu@mail.ru>
2023-01-13 19:05:07 +0500

    Create forms for password fields

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -1,40 +1,27 @@
-import django.contrib.auth.password_validation as validators
-from django.core import exceptions
 from rest_framework import serializers
 
+from core.forms import PasswordField
 from core.models import User
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(required=True)
-    password_repeat = serializers.CharField(required=False)
+    password = PasswordField(required=True)
+    password_repeat = PasswordField(required=True)
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password', 'password_repeat')
 
-    def is_valid(self, *, raise_exception=False):
-
+    def validate(self, attrs):
         # Проверка соответствия полей password и password_repeat
-        password_repeat = self.initial_data.pop('password_repeat')
-        if self.initial_data['password'] != password_repeat:
+        if attrs['password'] != attrs['password_repeat']:
             raise serializers.ValidationError(
                 "The two password fields didn't match.")
 
-        # Проверка надежности пароля
-        user = User(**self.initial_data)
-        errors = dict()
-        try:
-            validators.validate_password(password=password_repeat, user=user)
-        except exceptions.ValidationError as e:
-            errors['password'] = list(e.messages)
-
-        if errors:
-            raise serializers.ValidationError(errors)
-
-        return super().is_valid(raise_exception=raise_exception)
+        return attrs
 
     def create(self, validated_data):
+        del validated_data['password_repeat']
         user = super().create(validated_data)
 
         user.set_password(user.password)
