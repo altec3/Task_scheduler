3e73900b36064410c59bee6b6b98debbbe918048
Aleksey Lesnikov <poprobuyu@mail.ru>
2023-01-19 18:28:15 +0500

    Fix urls

--- todolist/goals/urls.py
+++ todolist/goals/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
 from goals.views import CategoryViewSet
 
-categories_router = routers.SimpleRouter()
-categories_router.register('goal_category', CategoryViewSet)
+category_create = CategoryViewSet.as_view({'post': 'create'})
+category_list = CategoryViewSet.as_view({'get': 'list'})
+category_detail = CategoryViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
 
 urlpatterns = [
-    path('', include(categories_router.urls)),
+    path('goal_category/create', category_create, name='category_create'),
+    path('goal_category/list', category_list, name='category_list'),
+    path('goal_category/<int:pk>', category_detail, name='category_detail'),
 ]
