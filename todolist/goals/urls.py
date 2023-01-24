0a9dff54da3d4d202ec65cd8c1978581fd5b6afc
Aleksey Lesnikov <poprobuyu@mail.ru>
2023-01-24 20:15:38 +0500

    Create custom router

--- todolist/goals/urls.py
+++ todolist/goals/urls.py
@@ -1,51 +1,23 @@
-from django.urls import path
+from django.urls import path, include
 
-from goals.views import CategoryViewSet, GoalViewSet, CommentViewSet, BoardViewSet
+from goals.routers import CustomAPIRouter
+from goals.views import BoardViewSet, CategoryViewSet, GoalViewSet, CommentViewSet
 
-category_create = CategoryViewSet.as_view({'post': 'create'})
-category_list = CategoryViewSet.as_view({'get': 'list'})
-category_detail = CategoryViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy',
-})
-goal_create = GoalViewSet.as_view({'post': 'create'})
-goal_list = GoalViewSet.as_view({'get': 'list'})
-goal_detail = GoalViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy',
-})
-comment_create = CommentViewSet.as_view({'post': 'create'})
-comment_list = CommentViewSet.as_view({'get': 'list'})
-comment_detail = CommentViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy',
-})
-board_create = BoardViewSet.as_view({'post': 'create'})
-board_list = BoardViewSet.as_view({'get': 'list'})
-board_detail = BoardViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy',
-})
+board_router = CustomAPIRouter(trailing_slash=False)
+board_router.register('board', BoardViewSet)
+
+category_router = CustomAPIRouter(trailing_slash=False)
+category_router.register('goal_category', CategoryViewSet)
+
+goal_router = CustomAPIRouter(trailing_slash=False)
+goal_router.register('goal', GoalViewSet)
+
+comment_router = CustomAPIRouter(trailing_slash=False)
+comment_router.register('goal_comment', CommentViewSet)
 
 urlpatterns = [
-    path('goal_category/create', category_create, name='category-create'),
-    path('goal_category/list', category_list, name='category-list'),
-    path('goal_category/<int:pk>', category_detail, name='category-detail'),
-    path('goal/create', goal_create, name='goal-create'),
-    path('goal/list', goal_list, name='goal-list'),
-    path('goal/<int:pk>', goal_detail, name='goal-detail'),
-    path('goal_comment/create', comment_create, name='comment-create'),
-    path('goal_comment/list', comment_list, name='comment-list'),
-    path('goal_comment/<int:pk>', comment_detail, name='comment-detail'),
-    path('board/create', board_create, name='board-create'),
-    path('board/list', board_list, name='board-list'),
-    path('board/<int:pk>', board_detail, name='board-detail'),
+    path('', include(board_router.urls)),
+    path('', include(category_router.urls)),
+    path('', include(goal_router.urls)),
+    path('', include(comment_router.urls)),
 ]
