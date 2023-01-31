05f389807ae22c13d5e5f317f26f1f20c5333d42
Aleksey Lesnikov <poprobuyu@mail.ru>
2023-01-31 16:51:52 +0500

    Create 'TgUser' model

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from core.models import User
+
+
+class TgUser(models.Model):
+
+    tg_id = models.SmallIntegerField(verbose_name='ID пользователя в Telegram')
+    tg_username = models.CharField(verbose_name='Имя пользователя в Telegram', max_length=255, null=True, blank=True)
+    verification_code = models.CharField(verbose_name='Код верификации', max_length=255)
+    user = models.ForeignKey(User, verbose_name='Пользователь', related_name='tg_user', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Telegram пользователь'
+        verbose_name_plural = 'Telegram пользователи'
+
+    def __str__(self):
+        return self.user.username
