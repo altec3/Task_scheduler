a37af385f2774d1b4648db0e451842dde926dc84
Aleksey Lesnikov <poprobuyu@mail.ru>
2023-01-31 17:04:11 +0500

    Add 'bot_tguser' table to admin site

--- todolist/bot/admin.py
+++ todolist/bot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from bot.models import TgUser
+
+
+@admin.register(TgUser)
+class TgUserAdmin(admin.ModelAdmin):
+    list_display = ('tg_id', 'tg_username', 'user',)
+    search_fields = ('user',)
