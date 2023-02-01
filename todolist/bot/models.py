7faa850a766856e1b16269cadb27209062cf0adc
Aleksey Lesnikov <poprobuyu@mail.ru>
2023-02-01 14:01:49 +0500

    Update 'TgUser' model

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -5,10 +5,15 @@ from core.models import User
 
 class TgUser(models.Model):
 
-    tg_id = models.SmallIntegerField(verbose_name='ID пользователя в Telegram')
+    tg_id = models.IntegerField(verbose_name='ID пользователя в Telegram')
     tg_username = models.CharField(verbose_name='Имя пользователя в Telegram', max_length=255, null=True, blank=True)
     verification_code = models.CharField(verbose_name='Код верификации', max_length=255)
-    user = models.ForeignKey(User, verbose_name='Пользователь', related_name='tg_user', on_delete=models.CASCADE)
+    user = models.ForeignKey(User,
+                             verbose_name='Пользователь',
+                             related_name='tg_user',
+                             null=True,
+                             on_delete=models.CASCADE
+                             )
 
     class Meta:
         verbose_name = 'Telegram пользователь'
