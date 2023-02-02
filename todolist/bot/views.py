a61ea843e03b0787d7a31b88cbe44efe6452f9f8
Aleksey Lesnikov <poprobuyu@mail.ru>
2023-02-02 10:35:14 +0500

    Create 'TgUserUpdateView'

--- todolist/bot/views.py
+++ todolist/bot/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import permissions, generics, mixins
 
-# Create your views here.
+from bot.models import TgUser
+from bot.serializers import TgUserSerializer
+
+
+class TgUserUpdateView(mixins.UpdateModelMixin, generics.GenericAPIView):
+    queryset = TgUser.objects.all()
+    model = TgUser
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = TgUserSerializer
+
+    # Переопределяем метод для получения объекта TgUser не по pk, а по полю 'verification_code'
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        filter_kwargs = {'verification_code': self.request.data['verification_code']}
+        obj = generics.get_object_or_404(queryset, **filter_kwargs)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    # Реализуем метод PATCH
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
