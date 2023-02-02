ba5cf19339c620e1ed57661c5e9cc3b44b006384
Aleksey Lesnikov <poprobuyu@mail.ru>
2023-02-02 10:49:16 +0500

    Modify 'TgUser' model

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -5,9 +5,9 @@ from core.models import User
 
 class TgUser(models.Model):
 
-    tg_id = models.IntegerField(verbose_name='ID пользователя в Telegram')
+    tg_id = models.IntegerField(verbose_name='ID пользователя в Telegram', unique=True)
     tg_username = models.CharField(verbose_name='Имя пользователя в Telegram', max_length=255, null=True, blank=True)
-    verification_code = models.CharField(verbose_name='Код верификации', max_length=255)
+    verification_code = models.CharField(verbose_name='Код верификации', max_length=255, unique=True)
     user = models.ForeignKey(User,
                              verbose_name='Пользователь',
                              related_name='tg_user',
